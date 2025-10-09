d0fdcfa14e2f038e87b5b8884ef30594ddf0b3ef
“Yuanchongyu” <chongyu@matt3r.ai>
2025-10-09 13:04:57 -0700

    add Egolane annotation tool

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import AnnotationTool from './pages/AnnotationTool';
 import ObjectDetectionTool from './pages/ObjectDetectionTool';
+import EgoLaneAnnotationTool from './pages/EgoLaneAnnotationTool';
 import ScenarioAnalysisTool from './pages/ScenarioAnalysisTool';
 import Video2Everything from './pages/Video2Everything';
 import VlmAnalysisTool from './pages/VlmAnalysisTool';
@@ -14,6 +15,7 @@ function AppRouter() {
         <Route path="/" element={<HomePage />} />
         <Route path="/annotation-tool" element={<AnnotationTool />} />
         <Route path="/object-detection" element={<ObjectDetectionTool />} />
+        <Route path="/ego-lane-annotation" element={<EgoLaneAnnotationTool />} />
         <Route path="/scenario-analysis" element={<ScenarioAnalysisTool />} />
         <Route path="/video2everything" element={<Video2Everything />} />
         <Route path="/vlm-analysis" element={<VlmAnalysisTool />} />
